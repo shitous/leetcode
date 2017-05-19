f4bbfa28546eb8b0414f7122623f456f665f85e7
shitous <sunjingshi@gmail.com>
2017-05-19 18:17:47 -0400

    Update single-number.java

--- Week1_interview_recursion/single-number.java
+++ Week1_interview_recursion/single-number.java
@@ -1,22 +1,15 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set1 = new HashSet<Integer>();
-        HashSet<Integer> set2 = new HashSet<Integer>();
-        for (int i =0; i<nums.length; i++){
-            if (set1.contains(nums[i])){
-                set2.add(nums[i]);
-            }
-            else{
-                set1.add(nums[i]);
-            }
-        }
-        int result = 0;
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
         for (int i =0; i< nums.length; i++){
-            if (!set2.contains(nums[i])){
-                result = nums[i];
-            }
+            if(set1.contains(nums[i])) set2.add(nums[i]);
+            else set1.add(nums[i]);
         }
-        return result;
-        
+        int res = 0;
+        for (int i =0; i< nums.length; i++){
+            if (!set2.contains(nums[i])) res = nums[i];
+        }
+        return res;
     }
 }
