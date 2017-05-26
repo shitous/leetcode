24c01487f1ad3bd25b7915488b32c8e3dd24c0fb
Jingshi <js4479@georgetown.edu>
2017-05-26 16:03:44 -0400

    week 2

--- Week2/Medium/search-for-a-range.java
+++ Week2/Medium/search-for-a-range.java
@@ -1,26 +1,17 @@
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l = nums.length-1;
-        int r = 0;
-        int low = 0;
+        int low = 0; 
         int high = nums.length-1;
-        while (low <= high){
-            int mid = (low+high)>>>1;
+        while(low <= high){
+            int mid = (low+ high)>>>1;
             if(nums[mid]==target){
-                if(mid < l) l = mid;
-                if(mid > r) r = mid;
-                if(nums[low]==target) l = low;
-                if(nums[high]==target) r = high;
-                if(nums[low]==target && nums[high]==target){
-                    return new int [] {l,r};
-                } 
-                if(nums[low]<target) low++;
-                if(nums[high]>target) high--;
+                if(nums[low]==target && nums[high]==target) return new int [] {low, high};
+                if(nums[low]<target) low ++;
+                if(nums[high]>target) high --;
             }
             else if (nums[mid]<target) low = mid+1;
             else high = mid -1;
         }
-        return new int [] {-1,-1};
-        
+        return new int [] {-1, -1};
     }
 }
