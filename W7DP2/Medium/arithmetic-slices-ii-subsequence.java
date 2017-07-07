5bfbb896d4fe9a4d42b816e4c5109b9d01d993bd
shitous <sunjingshi@gmail.com>
2017-07-07 09:40:37 -0400

    Update arithmetic-slices-ii-subsequence.java

--- W7DP2/Medium/arithmetic-slices-ii-subsequence.java
+++ W7DP2/Medium/arithmetic-slices-ii-subsequence.java
@@ -6,7 +6,7 @@ public class Solution {
         for (int i = 0; i < n; i++) {
             map[i] = new HashMap<>();
             for (int j = 0; j < i; j++) {
-                System.out.print("(" + i +" "+j + ")");
+                //System.out.print("(" + i +" "+j + ")");
                 long dif = (long)A[i] - A[j];
                 if (dif <= Integer.MIN_VALUE || dif >= Integer.MAX_VALUE) {
                     continue;
@@ -15,10 +15,10 @@ public class Solution {
                 int c1 = map[i].getOrDefault(d, 0);
                 int c2 = map[j].getOrDefault(d, 0);
                 res += c2;
-                System.out.print(" + "+c2);
+                //System.out.print(" + "+c2);
                 map[i].put(d, c1 + c2 + 1);
             }
-            System.out.println(map[i]);
+            //System.out.println(map[i]);
         }
         
         return res;
